d736770a1928a5cb2fc0142879d4d230a3b049c3
Leonardo Russo <info@lrusso.com>
2021-11-28 18:23:18 -0300

    Add files via upload

--- phaser.js
+++ phaser.js
@@ -22685,7 +22685,7 @@ var Phaser = Phaser || {
     * @constant
     * @type {string}
     */
-    VERSION: '2.4.4',
+    VERSION: '2.4.4-LRusso',
 
     /**
     * An array of Phaser game instances.
